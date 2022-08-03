274c2a1911a638a1a8efb7b9f2764af8b0ef5beb
Raul - Anpad <raul_sigoli@hotmail.com>
2022-08-03 15:57:57 -0300

    Styled Components
    Nesse projeto, nós vamos utilizar o Styled Components para criar as estilizações da nossa aplicação.
    
    O Styled Components é uma biblioteca de CSS-in-JS que permite que a gente utilize o CSS dentro do JavaScript e em um formato parecido com os do React, adicionando muitas funcionalidades à estilização da nossa aplicação.
    
    Vamos ver nessa aula como configurar o Styled Components, seus principais conceitos e começar a estrutura de estilização da aplicação.

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,8 +1,15 @@
+import { Button } from "./components/Button";
 
 
 export function App() {
   return (
-    <h1>hello World</h1>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
   )
 }
 
